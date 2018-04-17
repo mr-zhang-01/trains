49003da21f23c545b1c106129b21b96de615e835
marx <18801264023@163.com>
2018-04-17 16:58:13 +0800

    main modify

--- src/main.go
+++ src/main.go
@@ -1,7 +1,20 @@
 package main
 
+/*
+	车站系统
+*/
+
 import "fmt"
+import (
+	_ "vendors/mysql"
+	"database/sql"
+)
 
 func main(){
-	fmt.Println("hello world!!!!!!!!!!!!")
+	fmt.Println("hello world!!!!!")
+	db, err := sql.Open("mysql", "root:920717@/mrzhang")
+
+	if err == nil{
+		fmt.Println(db)
+	}
 }
